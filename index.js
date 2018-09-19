84f832d1f60b2f260082a270003335b8cee26658
Andrew Balanuk <abalanuk@lohika.com>
2018-09-19 10:01:21 -0700

    Starting http server to listen on port

---
+++ index.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const server = http.createServer(function(req, res){
+  res.end("TEST\n");
+});
+
+server.listen(3000, function(){
+  console.log("Server is listening on port 3000")
+});
