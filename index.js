8d137d025d488236a8ff70c01165c040c1ccbda7
Andrew Balanuk <abalanuk@lohika.com>
2018-09-19 10:26:57 -0700

    Get trimmed path of requested Url + HTTP method

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer(function(req, res){
+  // Get the URL and parse it
+
+  // Get the path from URL
+  const parsedUrl = url.parse(req.url, true);
+
+  const {pathname} = parsedUrl
+  const trimmedPath = pathname.replace(/^\/+|\/+$/g,'')
+
+  // Get the HTTP method
+  const {method} = req
+
+  // Send the response
   res.end("TEST\n");
+
+  // Log the path user requested
+  console.log("Requested path: " + trimmedPath);
+  console.log("With method: " + method);
 });
 
 server.listen(3000, function(){
