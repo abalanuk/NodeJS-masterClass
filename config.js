2a187ce43a5b405a8a61d05e33b250eeb722b0d7
Andrew Balaniuk <andrewbalanuk@gmail.com>
2019-10-09 15:46:09 +0300

    Edit config for Twilio integration, add testing twillio Api call

--- config.js
+++ config.js
@@ -6,7 +6,12 @@ environments.staging = {
   'httpsPort': 3001,
   'envName': 'staging',
   'hashingSecret': 'thisIsASecret',
-  'maxChecks': 5
+  'maxChecks': 5,
+  'twilio': {
+    'accountSid': 'ACbfff9f1164da13f39d6c53a5eeccb54c',
+    'authToken': '14907cd4394326e5a52526b9a637ce3c',
+    'fromPhone': '+18637222924'
+  }
 };
 
 environments.production = {
@@ -14,7 +19,12 @@ environments.production = {
   'httpsPort': 5001,
   'envName': 'production',
   'hashingSecret': 'thisIsASecret',
-  'maxChecks': 5
+  'maxChecks': 5,
+  'twilio': {
+    'accountSid': 'ACbfff9f1164da13f39d6c53a5eeccb54c',
+    'authToken': '14907cd4394326e5a52526b9a637ce3c',
+    'fromPhone': '+18637222924'
+  }
 };
 
 // Determine which environment was passed as a comand-line argument
