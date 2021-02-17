6137d65f4f61907be15f109a645ea382c7f835ef
Andrii Balaniuk <andrii.balaniuk@digicert.com>
2021-02-17 16:06:14 +0200

    Last commit

--- README.md
+++ README.md
@@ -2,26 +2,33 @@
 Node.js Master Class
 https://pirple.thinkific.com/courses/take/the-nodejs-master-class
 
-1. An "uptime monitor" allows users to enter URLs they want monitored, and receive alerts when those resources "go down" or "come back up"
-2. API allows create/edit/delete user
-3. User can "sign in" and get the token for future authenticated requests
-4. Allows user to "sign out" which invalidates their token
+# Common idea is creating http/https server which listens on configured port and by callback is processing data by req/resp objects
+# By "path" got from "req" object we are making decision whether we have handler mapped to route in router
+# If handler exists, we are calling it with data formed from "req" object and providing callback function declaration according to business logic which accepts 
+# data and optional payload. Then we are calling method tied to parent path's handler and specified method in "data" to do some business logic(write/update some data into file)
+
+# First App: an "uptime monitor" allows users to enter URLs they want monitored, and receive alerts when those resources "go down" or "come back up"
+1. API allows create/edit/delete user
+2. User can "sign in" and get the token for future authenticated requests
+4. User can "sign out" which invalidates their token
 5. Signed-in user using token can create a "check": task to check a given URL if it is up or down.
 6. Signed-in user using token can edit/delete their checks but only limited number.
-7. Each "check" should performed in the background(at the appropriate time) and alerts to the user when a check changes its state from "up" to "down"
-8.  
+7. Each "check" should be performed in the background(at the appropriate time) and alerts to the user when a check changes its state from "up" to "down"
+
 #### Features: 
-- user sign-up/sign in 
+- user sign-up/sign in
+- user sign out
+- create/edit/delete user
+- create/edit/delete check for a given URL
 - send SMS alert to user
 
 
-@TODO: 
+# Running App:
+NODE_ENV=staging node index.js or simply node index.js
 
+@TODO: 
 - add jshint|jslint
 - create a real schema/tables in DB and connect API to get/put data into it 
-
-
-
 
 ### Test data:
 {
@@ -34,7 +41,7 @@ https://pirple.thinkific.com/courses/take/the-nodejs-master-class
 
 ### Concepts
 
-Errback:
+Error back:
 exampleFunction(function(err, data)) {
     // check the error
     // do stuff with the data
--- index.js
+++ index.js
@@ -2,21 +2,101 @@ const http = require('http');
 const https = require('https');
 const url = require('url');
 const stringDecoder = require('string_decoder').StringDecoder;
-const config = require('./config');
 const fs = require('fs');
-const _data = require('./lib/data');
+
+const config = require('./config');
+// const _data = require('./lib/data');
 const helpers = require('./lib/helpers');
 const handlers = require('./lib/handlers');
 
-//Testing data library...
+// Testing data storing library...
 // _data.delete('test', 'newFile', function(err){
-//   console.log('this was the error', err);
+//   console.log('this was the error:', err);
 // });
+// _data.create('test', 'newFile', {'title': "Let's write cool code"}, function(err){
+//   console.log('this was the error:', err);
+// });
+// _data.read('test', 'newFile', function(err, data){
+//   console.log('this was the error:', err, '/ this was data:', data);
+// });
+// _data.update('test', 'newFile', {'title': "Let's update cool code"}, function(err){
+//   console.log('this was the error:', err);
+// });
+
 
 //Testing twilio API
 // helpers.sendTwilioSms('+380976503796', 'Hello!', function(err){
 //   console.log(err);
 // });
+
+// All the server logic for both http and https server
+const unifiedServerCallback = function(req, res) {
+    // Get the URL and parse it
+    // Get the pathname from URL
+    const parsedUrl = url.parse(req.url, true); // 'true' argument tells to 'parse' method to process query string like ?id=1 to object query: {id: 1}
+    // console.log('parsedUrl::', JSON.stringify(parsedUrl));
+
+    const {pathname} = parsedUrl;
+    const trimmedPath = pathname.replace(/^\/+|\/+$/g,''); // like 'users' or 'ping'
+    // console.log('trimmedPath::', trimmedPath);
+
+    // Get the HTTP method
+    const {method} = req;
+
+    // Get the query string as an object
+    const {query} = parsedUrl;
+
+    // Get the headers as an object
+    const {headers} = req;
+
+    // Get the payload if any, by concat chunks of decoded data
+    const decoder = new stringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', function(data){
+        buffer += decoder.write(data);
+    })
+
+    req.on('end', function(){
+        buffer += decoder.end();
+
+        // Choose the handler this request should go to
+        const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath]
+            : handlers.notFound;
+
+        // Construct data object to send to the handler
+        const data = {
+            'trimmedPath': trimmedPath,
+            'query': query,
+            'method': method,
+            'headers': headers,
+            'payload': helpers.parseJsonToObject(buffer)
+        }
+
+        chosenHandler(data, function(statusCode, payload){
+            // Use the status code or the default 200
+            statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+            // Use the payload or the default {}
+            payload = typeof(payload) === 'object' ? payload : {};
+
+            const payloadString = JSON.stringify(payload);
+
+            // Send the response
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(statusCode);
+            res.end(payloadString);
+
+            console.log("Returned response: \n", statusCode, payloadString);
+        })
+    })
+
+    // Log the path user requested
+    console.log("Requested path: " + trimmedPath);
+    /*
+    console.log("With method: " + method);
+    console.log("Query object is: \n" + JSON.stringify(query));
+    console.log("Request headers: \n" + JSON.stringify(headers));
+    */
+}
 
 // Instantiate http server
 const httpServer = http.createServer(function(req, res){
@@ -39,73 +119,6 @@ const httpsServer = https.createServer(httpsServerOptions, function(req, res){
 httpsServer.listen(config.httpsPort, function(){
   console.log("HTTPS server is listening on port " + config.httpsPort + " in "+ config.envName);
 });
-
-// All the server logic for both http and https server
-const unifiedServerCallback = function(req, res) {
-    // Get the URL and parse it
-    // Get the path from URL
-    const parsedUrl = url.parse(req.url, true);
-
-    const {pathname} = parsedUrl;
-    const trimmedPath = pathname.replace(/^\/+|\/+$/g,'');
-
-    // Get the HTTP method
-    const {method} = req;
-
-    // Get the query string as an object
-    const {query} = parsedUrl;
-
-    // Get the headers as an object
-    const {headers} = req;
-
-    // Get the payload if any
-    const decoder = new stringDecoder('utf-8');
-    let buffer = '';
-    req.on('data', function(data){
-      buffer += decoder.write(data);
-    })
-
-    req.on('end', function(){
-      buffer += decoder.end();
-
-      //Choose the handler this request should go to
-      const chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ?
-      router[trimmedPath] : handlers.notFound;
-
-      // Construct data object to send to the handler
-      data = {
-        'trimmedPath': trimmedPath,
-        'query': query,
-        'method': method,
-        'headers': headers,
-        'payload': helpers.parseJsonToObject(buffer)
-      }
-
-      chosenHandler(data, function(statusCode, payload){
-          // Use the status code or the default 200
-          statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
-          // Use the payload or the default {}
-          payload = typeof(payload) === 'object' ? payload : {};
-
-          const payloadString = JSON.stringify(payload);
-
-          // Send the response
-          res.setHeader('Content-Type', 'application/json');
-          res.writeHead(statusCode);
-          res.end(payloadString);
-
-          console.log("Returned response: \n", statusCode, payloadString);
-      })
-    })
-
-    // Log the path user requested
-    console.log("Requested path: " + trimmedPath);
-    /*
-    console.log("With method: " + method);
-    console.log("Query object is: \n" + JSON.stringify(query));
-    console.log("Request headers: \n" + JSON.stringify(headers));
-    */
-}
 
 // Define a request router
 router = {
